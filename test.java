8854b473604e2ea146ae6690afb1a244296e5d43
GuArDiAn1611 <vivekchdry@gmail.com>
2020-05-05 16:55:47 +0530

    multiple add

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 class test{
 	public static void main(String args[]){
-	System.out.println("Testing Git Commands");
-}
+		System.out.println("Testing Git Commands");
+		System.out.println("naya change");
+	}
 }
