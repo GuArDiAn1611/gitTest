c04b8a64f7191c0b1b612c3977cc965bcec7e3a1
GuArDiAn1611 <vivekchdry@gmail.com>
2020-05-09 19:22:41 +0530

    first changes

---
+++ src/main/java/com/devops/calc/Calculator.java
@@ -0,0 +1,35 @@
+package com.devops.calc;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator 
+{
+    public static void main(String[] args) {
+		System.out.println("1066 + 7 = " + add(1066, 7));
+		System.out.println("5667 - 234 = " + subtract(5667, 234));
+		System.out.println("12 * 134 = " + multiply(12, 134));
+		System.out.println("789 / 12 = " + divide(789, 12));
+	}
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+	
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+	public static double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+				throw new IllegalArgumentException("Divisor cannot divide by 0");
+		} else {
+			result = Double.valueOf(a) / Double.valueOf(b);
+		}
+		return result;
+	}
+}
