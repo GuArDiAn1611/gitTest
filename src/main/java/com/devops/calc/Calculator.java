9ca23ce9a05583023f85487ac995be42171db3c6
GuArDiAn1611 <vivekchdry@gmail.com>
2020-05-09 19:52:29 +0530

    new change

--- src/main/java/com/devops/calc/Calculator.java
+++ src/main/java/com/devops/calc/Calculator.java
@@ -11,6 +11,8 @@ public class Calculator
 		System.out.println("5667 - 234 = " + subtract(5667, 234));
 		System.out.println("12 * 134 = " + multiply(12, 134));
 		System.out.println("789 / 12 = " + divide(789, 12));
+		System.out.println("new change");
+		
 	}
 	
 	public static int add(int a, int b) {
